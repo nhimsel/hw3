ae2ce80e97faefbcf86d6bb2bf3a4f25b595abf7
nhimsel <>
2024-09-17 10:23:29 -0500

    part 2 completed

--- hw3.cpp
+++ hw3.cpp
@@ -17,5 +17,24 @@ int main()
         cout << "Enjoy the sunshine!" << endl;
     }
 
+    //Part 2
+    int x = 21;
+    int y = -2;
+
+    if (x>y)
+    {
+        cout << "x is greater than y" << endl;
+    }
+
+    else if (x==y)
+    {
+        cout << "x is equal to y" << endl;
+    }
+
+    else
+    {
+        cout << "x is less than y" << endl;
+    }
+
     return 0;
 }
