4169949afded08bd8679c15aeb65360ed787e5ea
nhimsel <nhimsel@cuw.edu>
2024-09-17 10:20:12 -0500

    Part 1 completed

---
+++ hw3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Part 1
+    bool isRainy = true;
+    
+    if (isRainy)
+    {
+        cout << "Bring an umbrella!" << endl;
+    }
+
+    else
+    {
+        cout << "Enjoy the sunshine!" << endl;
+    }
+
+    return 0;
+}
